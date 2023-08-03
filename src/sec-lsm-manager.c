1514e1dd318993af5ea4ca3eae83a5c17667069c
José Bollo <jose.bollo@iot.bzh>
2023-08-03 14:56:50 +0200

    sec-lsm-manager: Factorize code

--- src/sec-lsm-manager.c
+++ src/sec-lsm-manager.c
@@ -131,7 +131,8 @@ __nonnull() __wur static int send_reply(sec_lsm_manager_t *sec_lsm_manager, cons
     trial = 0;
     for (;;) {
         /* fill the fields */
-        for (i = rc = 0; i < count && rc == 0; i++) rc = prot_put_field(prot, fields[i]);
+        for (i = rc = 0; i < count && rc == 0; i++)
+	    rc = prot_put_field(prot, fields[i]);
 
         /* send if done */
         if (rc == 0) {
@@ -171,7 +172,7 @@ __nonnull() __wur static int send_reply(sec_lsm_manager_t *sec_lsm_manager, cons
  * @return  0 in case of success or a negative -errno value
  */
 __nonnull((1, 2)) __wur
-    static int putxkv(sec_lsm_manager_t *sec_lsm_manager, const char *command, const char *optarg, ...) {
+static int putxkv(sec_lsm_manager_t *sec_lsm_manager, const char *command, const char *optarg, ...) {
     int nf, rc;
     const char *fields[8] = {0};
 
@@ -363,6 +364,47 @@ __nonnull() __wur static int ensure_opened(sec_lsm_manager_t *sec_lsm_manager) {
     return sec_lsm_manager->fd < 0 ? connection(sec_lsm_manager) : 0;
 }
 
+/**
+ * @brief lock, open and send
+ *
+ */
+static int sync_send_do(sec_lsm_manager_t *sec_lsm_manager, int (*aftersend)(sec_lsm_manager_t *), ...)
+{
+    va_list va;
+    int nf, rc;
+    const char *fields[8];
+
+    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
+
+    /* check lock */
+    if (sec_lsm_manager->synclock)
+        return -EBUSY;
+
+    /* lock and open */
+    sec_lsm_manager->synclock = true;
+    rc = ensure_opened(sec_lsm_manager);
+    if (rc >= 0) {
+
+        /* fill the arguments */
+        va_start(va, aftersend);
+        for (nf = 0 ; nf < (int)(sizeof fields / sizeof *fields) ; nf++) {
+            fields[nf] = va_arg(va, const char *);
+            if (fields[nf] == NULL)
+                break;
+	}
+        va_end(va);
+
+        /* send the reply */
+        rc = send_reply(sec_lsm_manager, fields, nf);
+        if (rc >= 0) {
+            rc = aftersend(sec_lsm_manager);
+        }
+    }
+    sec_lsm_manager->synclock = false;
+
+    return rc;
+}
+
 /**********************/
 /*** PUBLIC METHODS ***/
 /**********************/
@@ -428,251 +470,100 @@ void sec_lsm_manager_disconnect(sec_lsm_manager_t *sec_lsm_manager) {
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_set_id(sec_lsm_manager_t *sec_lsm_manager, const char *id) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
     CHECK_NO_NULL(id, "id");
 
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = putxkv(sec_lsm_manager, _id_, id, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _id_, id, NULL);
 }
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_add_path(sec_lsm_manager_t *sec_lsm_manager, const char *path, const char *path_type) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
     CHECK_NO_NULL(path, "path");
     CHECK_NO_NULL(path_type, "path_type");
 
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _path_, path, path_type, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _path_, path, path_type, NULL);
 }
 
 /* see sec-lsm-manager.h */
 __nonnull() __wur
 int sec_lsm_manager_add_plug(sec_lsm_manager_t *sec_lsm_manager, const char *expdir, const char *impid, const char *impdir)
 {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
     CHECK_NO_NULL(expdir, "expdir");
     CHECK_NO_NULL(impid, "impid");
     CHECK_NO_NULL(impdir, "impdir");
 
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _plug_, expdir, impid, impdir, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _plug_, expdir, impid, impdir, NULL);
 }
-
-
-
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_add_permission(sec_lsm_manager_t *sec_lsm_manager, const char *permission) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
     CHECK_NO_NULL(permission, "permission");
 
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _permission_, permission, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _permission_, permission, NULL);
 }
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_clear(sec_lsm_manager_t *sec_lsm_manager) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
-
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _clear_, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _clear_, NULL);
 }
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_install(sec_lsm_manager_t *sec_lsm_manager) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
-
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _install_, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
-    return rc;
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _install_, NULL);
 }
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_uninstall(sec_lsm_manager_t *sec_lsm_manager) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
+    return sync_send_do(sec_lsm_manager, wait_done_or_error, _uninstall_, NULL);
+}
 
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-    rc = putxkv(sec_lsm_manager, _uninstall_, NULL);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_done_or_error(sec_lsm_manager);
-
-ret:
-    sec_lsm_manager->synclock = false;
+/**
+ * @brief callback for processing log replies
+ * @param sec_lsm_manager sec_lsm_manager client handler
+ * @return 0 if replied "done off", 1 if replied "done on" or a negative -errno value
+ */
+static int wait_log_reply(sec_lsm_manager_t *sec_lsm_manager)
+{
+    int rc = wait_done_or_error(sec_lsm_manager);
+    if (rc > 0)
+        rc = sec_lsm_manager->reply.count >= 2 && !strcmp(sec_lsm_manager->reply.fields[1], _on_);
     return rc;
 }
 
 /* see sec-lsm-manager.h */
 int sec_lsm_manager_log(sec_lsm_manager_t *sec_lsm_manager, int on, int off) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
-
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc >= 0) {
-        rc = putxkv(sec_lsm_manager, _log_, off ? _off_ : on ? _on_ : 0, NULL);
-        if (rc >= 0) {
-            rc = wait_done_or_error(sec_lsm_manager);
-            if (rc > 0)
-                rc = sec_lsm_manager->reply.count >= 2 && !strcmp(sec_lsm_manager->reply.fields[1], _on_);
-        }
-    }
-    sec_lsm_manager->synclock = false;
-
-    return rc < 0 ? rc : sec_lsm_manager->reply.count < 2 ? 0 : !strcmp(sec_lsm_manager->reply.fields[1], _on_);
+    return sync_send_do(sec_lsm_manager, wait_log_reply, _log_, off ? _off_ : on ? _on_ : NULL, NULL);
 }
 
-/* see sec-lsm-manager.h */
-int sec_lsm_manager_display(sec_lsm_manager_t *sec_lsm_manager) {
-    CHECK_NO_NULL(sec_lsm_manager, "sec_lsm_manager");
-
-    if (sec_lsm_manager->synclock)
-        return -EBUSY;
-
-    sec_lsm_manager->synclock = true;
-
-    int rc = ensure_opened(sec_lsm_manager);
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = putxkv(sec_lsm_manager, _display_, NULL);
-
-    if (rc < 0) {
-        goto ret;
-    }
-
-    rc = wait_reply(sec_lsm_manager, true);
-
+/**
+ * @brief callback for processing display replies
+ * @param sec_lsm_manager sec_lsm_manager client handler
+ * @return 0 on success or a negative -errno value
+ */
+static int wait_display_replies(sec_lsm_manager_t *sec_lsm_manager)
+{
+    int rc = wait_reply(sec_lsm_manager, true);
     if (rc > 2 && !strcmp(sec_lsm_manager->reply.fields[0], _string_)) {
         puts("################## SECURE APP ##################");
         do {
-            printf(&"%s %s %s\n"[rc > 3 ? 0 : 3],
+            printf(&"%s %s %s %s\n"[15 - 3 * (rc > 5 ? 5 : rc)],
               sec_lsm_manager->reply.fields[1],
               sec_lsm_manager->reply.fields[2],
-              rc > 3 ? sec_lsm_manager->reply.fields[3] : NULL);
+              rc > 3 ? sec_lsm_manager->reply.fields[3] : NULL,
+              rc > 4 ? sec_lsm_manager->reply.fields[4] : NULL);
             rc = wait_reply(sec_lsm_manager, true);
         }
         while (rc > 2 && !strcmp(sec_lsm_manager->reply.fields[0], _string_));
 
         puts("################################################");
-    } else if (rc > 0 && !strcmp(sec_lsm_manager->reply.fields[0], _error_)) {
-        ERROR("%s", sec_lsm_manager->reply.fields[1]);
     }
-
-ret:
-    sec_lsm_manager->synclock = false;
+    if (rc > 0 && !strcmp(sec_lsm_manager->reply.fields[0], _error_)) {
+        ERROR("%s", sec_lsm_manager->reply.fields[1]);
+	rc = -1;
+    }
     return rc;
+}
+
+/* see sec-lsm-manager.h */
+int sec_lsm_manager_display(sec_lsm_manager_t *sec_lsm_manager) {
+    return sync_send_do(sec_lsm_manager, wait_display_replies, _display_, NULL);
 }
